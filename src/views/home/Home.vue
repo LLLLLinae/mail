ac50e902e89d1c24115b68e4c34941dc2d4b2e62
郭凤琳 <915234557@qq.com>
2022-02-21 17:09:51 +0800

    完成首页navbar,轮播图，recommendView,featureView

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,47 @@
 <template>
-<div></div>
+  <div>
+    <nav-bar class="home-nav"><template slot="center">购物车</template></nav-bar>
+    <home-swiper :banner="banner"></home-swiper>
+    <recommend-view :recommend="recommend"></recommend-view>
+    <feature-view></feature-view>
+  </div>
 </template>
 
 <script>
+  import NavBar from "components/common/navbar/NavBar";
+  import homeSwiper from "./childComps/HomeSwiper";
+  import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
+
+  import {getHomeMultiData} from 'network/home'
 export default {
-name: "Home"
+name: "Home",
+  components:{NavBar,homeSwiper,RecommendView,FeatureView},
+  data(){
+    return{
+      banner:[],
+      recommend:[]
+    }
+  },
+  mounted() {
+  this.getHomeMultiData()
+  },
+  methods:{
+  getHomeMultiData(){
+    getHomeMultiData().then(
+      (res)=>{
+        this.recommend = res.data.recommend.list
+        this.banner = res.data.banner.list
+      }
+    )
+  }
+  }
 }
 </script>
 
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
