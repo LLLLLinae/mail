5bce5ebdfd1f5fafe429ec0e06c6f66b64c69ef8
LLLLLinae <915234557@qq.com>
2022-02-21 21:23:22 +0800

    完成tabControl

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,23 +4,31 @@
     <home-swiper :banner="banner"></home-swiper>
     <recommend-view :recommend="recommend"></recommend-view>
     <feature-view></feature-view>
+    <tab-control :titles="['流行', '新款', '精选']"></tab-control>
   </div>
 </template>
 
 <script>
-  import NavBar from "components/common/navbar/NavBar";
   import homeSwiper from "./childComps/HomeSwiper";
   import RecommendView from "./childComps/RecommendView";
   import FeatureView from "./childComps/FeatureView";
 
+
+  import NavBar from "components/common/navbar/NavBar";
+  import TabControl from "components/content/tabControl/TabControl";
   import {getHomeMultiData} from 'network/home'
 export default {
 name: "Home",
-  components:{NavBar,homeSwiper,RecommendView,FeatureView},
+  components:{NavBar,homeSwiper,RecommendView,FeatureView,TabControl},
   data(){
     return{
       banner:[],
-      recommend:[]
+      recommend:[],
+      goods:{
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []},
+      },
     }
   },
   mounted() {
